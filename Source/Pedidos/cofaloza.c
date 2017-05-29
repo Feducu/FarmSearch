86bef6f5558d597d5e9acc18530b9c3fe6806be4
Feducu <autotos@gmail.com>
2017-05-29 17:38:34 -0300

    Primera implementación del URL2FILE

--- Source/Pedidos/cofaloza.c
+++ Source/Pedidos/cofaloza.c
@@ -1,4 +1,21 @@
+
 #include <stdio.h>
+#include "structs.h"
+#include <string.h>
+
+/*
+https://www.cofaloza.com.ar/intranet/aplic/modules/carrito/shop.php?spx=999&buscar=$DATOS
+DATOS = Cdgo barras o nombre
+*/
+
+int cofaloza(){
+
+char buscar[15];
+char url[200]="https://www.cofaloza.com.ar/intranet/aplic/modules/carrito/shop.php?spx=999&buscar=";
+strcat(url,buscar);
+}
+
+
 
 
 
